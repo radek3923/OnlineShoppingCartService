3c1aac760181654c73b1ee083bb0fd888030efcb
Radoslaw Potocki <radoslaw.potocki3923@gmail.com>
2023-01-28 20:33:01 +0100

    Implement DataValidator

--- ShoppingCartServer/ShoppingCartServer/Utils/Validators/DataValidator.cs
+++ ShoppingCartServer/ShoppingCartServer/Utils/Validators/DataValidator.cs
@@ -1,6 +1,21 @@
-﻿namespace ShoppingCartServer.Utils.Validators;
+﻿using ShoppingCartServer.Interfaces;
 
-public class DataValidator
+namespace ShoppingCartServer.Utils.Validators;
+
+public class DataValidator : IEmailValidator, ILoginValidator, IPhoneNumberValidator
 {
-    
+    public bool isEmailValid()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool isLoginValid()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool isPhoneNumberValidatorValid()
+    {
+        throw new NotImplementedException();
+    }
 }
